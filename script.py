e1775841e0efe0a25a43cc05e8ee138783e4a787
Yaman Sigdel <Yamansigdel999@gmail.com>
2023-10-20 18:08:22 +0545

    page

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "This is home page"
+    return "Begins here !!"
+
+@app.route('/about')
+def about():
+    return render_template("home.html")
 
 if __name__=="__main__":
     app.run(debug=True)
